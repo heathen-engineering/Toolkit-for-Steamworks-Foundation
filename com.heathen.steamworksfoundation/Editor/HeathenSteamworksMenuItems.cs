48949cd8fb2a3f4f188df2acbcba7e3150c2a579
James McGhee <lodendsg@gmail.com>
2022-05-07 23:57:47 +0100

    Update HeathenSteamworksMenuItems.cs

--- com.heathen.steamworksfoundation/Editor/HeathenSteamworksMenuItems.cs
+++ com.heathen.steamworksfoundation/Editor/HeathenSteamworksMenuItems.cs
@@ -23,26 +23,6 @@ namespace HeathenEngineering.SteamworksIntegration.Editors
             while (!listProc.IsCompleted)
                 yield return null;
 
-            bool needsRemove = false;
-            if (listProc.Status == StatusCode.Success)
-            {
-                if (listProc.Result.Any(p => p.name == "com.heathen.steamworksfoundation"))
-                    needsRemove = true;
-            }
-            else
-                Debug.LogError("Failed to check Package Manager dependencies: " + listProc.Error.message);
-
-            if (needsRemove)
-            {
-                if (EditorUtility.DisplayDialog("Heathen Installer", "Steamworks Foundation appears to be installed and must be removed before continuing. Should we uninstall Steamworks Foundation now?", "Uninstall", "No"))
-                {
-                    var removeProc = Client.Remove("com.heathen.steamworksfoundation");
-
-                    while (!removeProc.IsCompleted)
-                        yield return null;
-                }
-            }
-
 #if !STEAMWORKS_NET && !HE_SYSCORE
             if (EditorUtility.DisplayDialog("Heathen Installer", "Steamworks.NET and System Core appear to be missing and are required for Steamworks Complete to work properly. Should we install Steamworks.NET and System Core now?", "Install", "No"))
             {

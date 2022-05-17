52f9f248c01bc9010c59ef046ea398edeedb1b3f
James McGhee <lodendsg@gmail.com>
2022-05-17 12:03:47 +0100

    re-added missing UI scripts
    These where incorrectly removed in the preivous update

---
+++ com.heathen.steamworksfoundation/Runtime/SetUserAvatar.cs
@@ -0,0 +1,114 @@
+﻿#if HE_SYSCORE && STEAMWORKS_NET
+using UnityEngine;
+using UnityEngine.Events;
+using Steamworks;
+using UserAPI = HeathenEngineering.SteamworksIntegration.API.User.Client;
+using FriendsAPI = HeathenEngineering.SteamworksIntegration.API.Friends.Client;
+
+namespace HeathenEngineering.SteamworksIntegration.UI
+{
+    /// <summary>
+    /// Applies the avatar of the indicated user to the attached RawImage
+    /// </summary>
+    [RequireComponent(typeof(UnityEngine.UI.RawImage))]
+    public class SetUserAvatar : MonoBehaviour
+    {
+        private UnityEngine.UI.RawImage image;
+        [SerializeField]
+        [Tooltip("Should the component load the local user's avatar on Start.\nIf false you must call LoadAvatar and provide the ID of the user to load")]
+        private bool useLocalUser;
+        public UnityEvent evtLoaded;
+
+        public UserData UserData
+        {
+            get
+            {
+                return currentUser;
+            }
+            set
+            {
+                LoadAvatar(value);
+            }
+        }
+
+        private UserData currentUser;
+
+        private void OnEnable()
+        {
+            FriendsAPI.EventPersonaStateChange.AddListener(HandlePersonaStateChange);
+        }
+
+        private void OnDisable()
+        {
+            FriendsAPI.EventPersonaStateChange.RemoveListener(HandlePersonaStateChange);
+        }
+
+        private void Start()
+        {
+            image = GetComponent<UnityEngine.UI.RawImage>();
+
+            if (useLocalUser)
+            {
+                var user = UserAPI.Id;
+                LoadAvatar(user);
+            }
+        }
+
+        private void HandlePersonaStateChange(PersonaStateChange_t arg)
+        {
+            if(FriendsAPI.PersonaChangeHasFlag(arg.m_nChangeFlags, EPersonaChange.k_EPersonaChangeAvatar)
+                && arg.m_ulSteamID == currentUser.SteamId)
+            {
+                UserData user = arg.m_ulSteamID;
+                if(user == currentUser)
+                {
+                    user.LoadAvatar((t) =>
+                    {
+                        image.texture = t;
+                        evtLoaded?.Invoke();
+                    });
+                }
+            }
+        }
+
+        public void LoadAvatar(UserData user) => user.LoadAvatar((r) =>
+        {
+            if(image == null)
+                image = GetComponent<UnityEngine.UI.RawImage>();
+
+            if (image == null)
+                return;
+
+            currentUser = user;
+            image.texture = r;
+            evtLoaded?.Invoke();
+        });
+
+        public void LoadAvatar(CSteamID user) => UserData.Get(user).LoadAvatar((r) =>
+        {
+            if (image == null)
+                image = GetComponent<UnityEngine.UI.RawImage>();
+
+            if (image == null)
+                return;
+
+            currentUser = user;
+            image.texture = r;
+            evtLoaded?.Invoke();
+        });
+
+        public void LoadAvatar(ulong user) => UserData.Get(user).LoadAvatar((r) =>
+        {
+            if (image == null)
+                image = GetComponent<UnityEngine.UI.RawImage>();
+
+            if (image == null)
+                return;
+
+            currentUser = user;
+            image.texture = r;
+            evtLoaded?.Invoke();
+        });
+    }
+}
+#endif
---
+++ com.heathen.steamworksfoundation/Runtime/TMProSetUserName.cs
@@ -0,0 +1,134 @@
+﻿#if HE_SYSCORE && STEAMWORKS_NET
+using UnityEngine;
+using Steamworks;
+using UserAPI = HeathenEngineering.SteamworksIntegration.API.User.Client;
+using FriendsAPI = HeathenEngineering.SteamworksIntegration.API.Friends.Client;
+
+namespace HeathenEngineering.SteamworksIntegration.UI
+{
+    /// <summary>
+    /// Applies the name of the indicated user to the attached label
+    /// </summary>
+    [RequireComponent(typeof(TMPro.TextMeshProUGUI))]
+    public class TMProSetUserName : MonoBehaviour
+    {
+        private TMPro.TextMeshProUGUI label;
+        [SerializeField]
+        [Tooltip("Should the component load the local user's name on Start.\nIf false you must call SetName and provide the ID of the user to load")]
+        private bool useLocalUser;
+
+        public bool ShowNickname
+        {
+            get
+            {
+                return showNickname;
+            }
+            set
+            {
+                showNickname = value;
+                SetName(currentUser);
+            }
+        }
+
+        [SerializeField]
+        [Tooltip("Should we show the profile name (set by the user this represents) or the nick name (set by the local user for this user)")]
+        private bool showNickname;
+
+        public UserData UserData
+        {
+            get
+            {
+                return currentUser;
+            }
+            set
+            {
+                SetName(value);
+            }
+        }
+
+        private UserData currentUser;
+
+        private void OnEnable()
+        {
+            FriendsAPI.EventPersonaStateChange.AddListener(HandlePersonaStateChange);
+        }
+
+        private void OnDisable()
+        {
+            FriendsAPI.EventPersonaStateChange.RemoveListener(HandlePersonaStateChange);
+        }
+
+        private void Start()
+        {
+            label = GetComponent<TMPro.TextMeshProUGUI>();
+
+            if (useLocalUser)
+            {
+                var user = UserAPI.Id;
+                SetName(user);
+            }
+        }
+
+        private void HandlePersonaStateChange(PersonaStateChange_t arg)
+        {
+            UserData user = arg.m_ulSteamID;
+            if ((FriendsAPI.PersonaChangeHasFlag(arg.m_nChangeFlags, EPersonaChange.k_EPersonaChangeName) || FriendsAPI.PersonaChangeHasFlag(arg.m_nChangeFlags, EPersonaChange.k_EPersonaChangeNickname))
+                && user == currentUser)
+            {
+                if (showNickname)
+                    label.text = user.Nickname;
+                else
+                    label.text = user.Name;
+            }
+        }
+
+        public void SetName(UserData user)
+        {
+            if (label == null)
+                label = GetComponent<TMPro.TextMeshProUGUI>();
+
+            if (label == null)
+                return;
+
+            currentUser = user;
+
+            if (showNickname)
+                label.text = user.Nickname;
+            else
+                label.text = user.Name;
+        }
+
+        public void SetName(CSteamID user)
+        {
+            if (label == null)
+                label = GetComponent<TMPro.TextMeshProUGUI>();
+
+            if (label == null)
+                return;
+
+            currentUser = user;
+
+            if (showNickname)
+                label.text = UserData.Get(user).Nickname;
+            else
+                label.text = UserData.Get(user).Name;
+        }
+
+        public void SetName(ulong user)
+        {
+            if (label == null)
+                label = GetComponent<TMPro.TextMeshProUGUI>();
+
+            if (label == null)
+                return;
+
+            currentUser = user;
+
+            if (showNickname)
+                label.text = UserData.Get(user).Nickname;
+            else
+                label.text = UserData.Get(user).Name;
+        }
+    }
+}
+#endif
